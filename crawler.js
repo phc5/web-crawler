bb84ef65fbc89ce98512dfbdae8e4bba6ebbde00
PaulChong <paulhyunchong@gmail.com>
2017-02-27 13:37:05 -0800

    crawl through page and its relative links

--- crawler.js
+++ crawler.js
@@ -2,25 +2,49 @@ var request = require('request'); // used to make HTTP requests
 var URL = require('url-parse'); // used to parse URLs
 var cheerio = require('cheerio'); // used to parse and select links on page
 
-console.log(process.argv[2]);
+var START_URL = process.argv[2];
+var pagesVisited = [];
+var pagesToVisit = [];
 
-const startingURL = process.argv[2];
+var isUrl = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/; // regex for URL check
 
-request(startingURL, (err, response, body) => {
-	if (err) {
-		console.log('Error: ' + error);
+if (isUrl.test(START_URL)) {
+	var url = new URL(process.argv[2]);
+	var baseURL = url.protocol + '//' + url.hostname;
+	console.log(baseURL);
+	pagesToVisit.push(START_URL);
+	crawl();
+} else {
+	console.log('Given string is not a URL. Please check arg 2');
+}
+
+function crawl() {
+	var nextPage = pagesToVisit.pop();
+	if (nextPage in pagesVisited) {
+		crawl();
+	} else {
+		visitPage(nextPage, crawl);
 	}
+}
 
-	if (response.statusCode === 200) {
+function visitPage(url, callback) {
+	pagesVisited[url] = true;
+	console.log("Visiting page " + url);
+	request(url, function(error, response, body) {
+		if (response.statusCode !== 200) {
+			callback();
+			return;
+		}
 		var $ = cheerio.load(body);
-		console.log('Title: ' + $('title').text());
-		// var allRelativeLinks = [];
-
-		// var relativeLinks = $("a[href^='/']");
-		// relativeLinks.each(() => {
-		// 	allRelativeLinks.push($(this).attr('href'));
-		// });
-
-		// console.log('Found ' + allRelativeLinks.length + ' relative links');
-	}
-});
+		var relativeLinks = $("a[href^='/']");
+		console.log('Found ' + relativeLinks.length + ' relative links on page');
+		relativeLinks.each(function() {
+			if ($(this).attr('href')[0] + $(this).attr('href')[1] !== '//') {
+				console.log($(this).attr('href'));
+				pagesToVisit.push(baseURL + $(this).attr('href'));
+			}
+			
+		})
+		callback();
+	})
+}
