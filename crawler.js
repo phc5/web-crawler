a77beb871dc419b22ae94321c4d61cfda7e341c5
PaulChong <paulhyunchong@gmail.com>
2017-02-27 20:28:09 -0800

    now gets static assets (stylesheets, images, and javascript) for every relative link

--- crawler.js
+++ crawler.js
@@ -2,28 +2,38 @@ var request = require('request'); // used to make HTTP requests
 var URL = require('url-parse'); // used to parse URLs
 var cheerio = require('cheerio'); // used to parse and select links on page
 
-var START_URL = process.argv[2];
+var startURL = process.argv[2];
 var pagesVisited = [];
 var pagesToVisit = [];
+var resultArray = [];
+var pageVisited = 0;
 
 var isUrl = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/; // regex for URL check
 
-if (isUrl.test(START_URL)) {
+if (isUrl.test(startURL)) {
 	var url = new URL(process.argv[2]);
 	var baseURL = url.protocol + '//' + url.hostname;
-	console.log(baseURL);
-	pagesToVisit.push(START_URL);
+	pagesToVisit.push(startURL);
 	crawl();
+
 } else {
-	console.log('Given string is not a URL. Please check arg 2');
+	console.log('Error: Argument 2 is not a URL. Please include protocol and resource. For example: http://example.com');
 }
 
 function crawl() {
-	var nextPage = pagesToVisit.pop();
-	if (nextPage in pagesVisited) {
-		crawl();
+	if (pagesToVisit.length !== 0) {
+		var nextPage = pagesToVisit.pop();
+		if (nextPage in pagesVisited) {
+			crawl();
+		} else {
+			resultArray.push({
+				url: nextPage,
+				assets: []
+			})
+			visitPage(nextPage, crawl);
+		}
 	} else {
-		visitPage(nextPage, crawl);
+		console.log(resultArray);
 	}
 }
 
@@ -31,20 +41,82 @@ function visitPage(url, callback) {
 	pagesVisited[url] = true;
 	console.log("Visiting page " + url);
 	request(url, function(error, response, body) {
-		if (response.statusCode !== 200) {
+		if (error || response.statusCode !== 200) {
 			callback();
 			return;
 		}
 		var $ = cheerio.load(body);
+
+		getImages(body);
+		getJS(body);
+		getStylesheets(body);
+
 		var relativeLinks = $("a[href^='/']");
-		console.log('Found ' + relativeLinks.length + ' relative links on page');
 		relativeLinks.each(function() {
+			var sameLink = baseURL + $(this).attr('href');
 			if ($(this).attr('href')[0] + $(this).attr('href')[1] !== '//') {
-				console.log($(this).attr('href'));
 				pagesToVisit.push(baseURL + $(this).attr('href'));
 			}
 			
 		})
+		pageVisited++;
 		callback();
 	})
+}
+
+function getImages(body) {
+	var $ = cheerio.load(body);
+	var imgLinks = $('img');
+	if (imgLinks.length !== 0) {
+		imgLinks.each(function() {
+			if ($(this).attr('src')) {
+				if ($(this).attr('src')[0] + $(this).attr('src')[1] === '//') {
+					resultArray[pageVisited].assets.push($(this).attr('src').slice(2));
+				} else if ($(this).attr('src')[0] === '/') {
+					resultArray[pageVisited].assets.push(baseURL + $(this).attr('src'));
+					
+				} else {
+					resultArray[pageVisited].assets.push($(this).attr('src'));
+				}
+			}
+		});
+	}
+}
+
+function getJS(body) {
+	var $ = cheerio.load(body);
+	var jsLinks = $('script');
+	if (jsLinks.length !== 0) {
+		jsLinks.each(function() {
+			if ($(this).attr('src')) {
+				if ($(this).attr('src')[0] + $(this).attr('src')[1] === '//') {
+					resultArray[pageVisited].assets.push($(this).attr('src').slice(2));
+				} else if ($(this).attr('src')[0] === '/') {
+					resultArray[pageVisited].assets.push(baseURL + $(this).attr('src'));
+				} else {
+					resultArray[pageVisited].assets.push($(this).attr('src'));
+				}
+			}
+		});
+	}
+}
+
+function getStylesheets(body) {
+	var $ = cheerio.load(body);
+	var stylesheetLinks = $('link');
+	if (stylesheetLinks !== 0) {
+		stylesheetLinks.each(function() {
+			if ($(this).attr('href') && $(this).attr('rel') === 'stylesheet') {
+				if ($(this).attr('href')[0] + $(this).attr('href')[1] === '//') {
+					resultArray[pageVisited].assets.push($(this).attr('href').slice(2));
+				} else if ($(this).attr('href')[0] === '/') {
+					resultArray[pageVisited].assets.push(baseURL + $(this).attr('href'));
+				} else if ($(this).attr('href')[0] + $(this).attr('href')[1] === '..') {
+					resultArray[pageVisited].assets.push(baseURL + $(this).attr('href').slice(2));
+				} else {
+					resultArray[pageVisited].assets.push($(this).attr('href'));
+				}
+			}
+		})
+	}
 }
